1c3f14c1df512fe66bed8fa02153e83541d90e1e
yunlou <yunlou@bcbook.cn>
2018-12-06 08:33:37 +0800

    添加缩放的位置修正。

--- client/src/index.js
+++ client/src/index.js
@@ -8,11 +8,12 @@ let cache = {
     fillStyle: "#fff",
     strokeStyle: "#000",
     lineWidth: 2,
-    eraserSize: 24,
-    ctxScale: 1,
-    ctxTranslateX: 0,
-    ctxTranslateY: 0
+    eraserSize: 24
 };
+let isMobile = (navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i));
+let ctxScale = 1;
+let ctxTranslateX = 0;
+let ctxTranslateY = 0;
 let points = [];
 let util = {
     fillCtx: function() {
@@ -22,54 +23,56 @@ let util = {
 };
 canvas.width = cache.ctxWidth;
 canvas.height = cache.ctxHeight;
-ctx.scale(cache.ctxScale, cache.ctxScale);
 //背景色
 util.fillCtx();
 
+function getPos(event) {
+    if(isMobile) {
+        event = event.changedTouches[0];
+    }
+
+    return  {
+        x: event.clientX,
+        y: event.clientY
+    };
+}
 function drawLine(beginPoint, controlPoint, endPoint) {
-    let translateX = cache.ctxTranslateX;
-    let translateY = cache.ctxTranslateY;
-    let scale = cache.ctxScale;
     ctx.beginPath();
     ctx.strokeStyle = cache.strokeStyle;
     ctx.lineWidth = cache.lineWidth;
-    ctx.moveTo(beginPoint.x/scale - translateX, beginPoint.y/scale - translateY);
-    ctx.quadraticCurveTo(controlPoint.x/scale - translateX, controlPoint.y/scale - translateY
-        , endPoint.x/scale - translateX, endPoint.y/scale - translateY);
+    ctx.moveTo(beginPoint.x/ctxScale - ctxTranslateX, beginPoint.y/ctxScale - ctxTranslateY);
+    ctx.quadraticCurveTo(controlPoint.x/ctxScale - ctxTranslateX, controlPoint.y/ctxScale - ctxTranslateY
+        , endPoint.x/ctxScale - ctxTranslateX, endPoint.y/ctxScale - ctxTranslateY);
     ctx.stroke();
     ctx.closePath();
 }
 function createCtxTransformStyleText() {
-    let scale = cache.ctxScale;
-    return "scale("+scale+","+scale+") translateX("+(cache.ctxTranslateX)+"px) translateY("+cache.ctxTranslateY+"px)";
+    return "scale("+ctxScale+","+ctxScale+") translateX("+(ctxTranslateX)+"px) translateY("+ctxTranslateY+"px)";
 }
 // 绑定事件
-let startPos = null,
-    beginPos = null,
+let startPos = [],
     operateType = "draw";
-$(canvas).on("touchstart", function(e) {
-    let touch = e.originalEvent.targetTouches[0];
-    startPos = {
-        x: touch.clientX,
-        y: touch.clientY
-    };
+$(canvas).on("touchstart mousedown", function(e) {
+    startPos[0] = getPos(e.originalEvent);
 
     switch (operateType) {
         case "draw":
-            points.push(startPos);
+            points.push(startPos[0]);
             break;
         case "eraser":
             let eraserSize = cache.eraserSize;
-            ctx.clearRect(startPos.x-eraserSize/2, startPos.y-eraserSize/2, cache.eraserSize, cache.eraserSize);
+            ctx.clearRect(startPos[0].x/ctxScale-ctxTranslateX-eraserSize/2
+                , startPos[0].y/ctxScale-ctxTranslateY-eraserSize/2
+                , cache.eraserSize, cache.eraserSize);
             break;
     }
-}).on("touchmove", function(e) {
-    let touch = e.originalEvent.targetTouches[0];
+}).on("touchmove mousemove", function(e) {
+    if(!startPos.length) {
+        return;
+    }
+
     e.preventDefault();
-    let newPos = {
-        x: touch.clientX,
-        y: touch.clientY
-    };
+    let newPos = getPos(e.originalEvent);
     switch (operateType) {
         case "draw":
             points.push(newPos);
@@ -80,26 +83,26 @@ $(canvas).on("touchstart", function(e) {
                     x: (lastTwoPoints[0].x + lastTwoPoints[1].x) / 2,
                     y: (lastTwoPoints[0].y + lastTwoPoints[1].y) / 2,
                 }
-                drawLine(startPos, controlPoint, endPoint);
-                startPos = endPoint;
+                drawLine(startPos[0], controlPoint, endPoint);
+                startPos[0] = endPoint;
             }
             break;
         case "eraser":
             let eraserSize = cache.eraserSize;
-            ctx.clearRect(newPos.x-eraserSize/2, newPos.y-eraserSize/2, cache.eraserSize, cache.eraserSize);
+            ctx.clearRect(newPos.x/ctxScale-ctxTranslateX-eraserSize/2
+                , newPos.y/ctxScale-ctxTranslateY-eraserSize/2
+                , cache.eraserSize, cache.eraserSize);
             break;
         case "move":
-            let translateX = cache.ctxTranslateX + newPos.x - startPos.x;
-            let translateY = cache.ctxTranslateY + newPos.y - startPos.y;
+            ctxTranslateX = ctxTranslateX + newPos.x - startPos[0].x;
+            ctxTranslateY = ctxTranslateY + newPos.y - startPos[0].y;
 
-            cache.ctxTranslateX = translateX;
-            cache.ctxTranslateY = translateY;
             canvas.style.webkitTransform = createCtxTransformStyleText();
-            startPos = newPos;
+            startPos[0] = newPos;
             break;
     }
-}).on("touchend", function(e) {
-    startPos = null;
+}).on("touchend mouseup", function(e) {
+    startPos.length = 0;
     points = [];
 });
 $(".color-list").on("click", "li", function() {
@@ -116,10 +119,14 @@ $(".eraser-size-list").on("click", "li", function() {
 });
 $(".scale-list").on("click", "li", function() {
     $(this).addClass("active").siblings().removeClass("active");
-    let ctxScale = +this.getAttribute("data-scale");
-    cache.ctxScale = ctxScale;
+    ctxScale = +this.getAttribute("data-scale");
+    if(ctxScale == 1) {
+        ctxTranslateX = ctxTranslateY = 0;
+    }
+
     canvas.style.webkitTransform = createCtxTransformStyleText();
 });
-$("#operateType").on("change", function() {
-    operateType = this.value;
+$(".operate-list").on("click", "li", function() {
+    $(this).addClass("active").siblings().removeClass("active");
+    operateType = this.getAttribute("data-type");
 });
